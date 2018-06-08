d63a310eeecb54f5e6751fe5dee6ba465bb2938a
WangWei0905 <750303420@qq.com>
2018-06-08 16:39:45 +0800

    代码调整

--- src/main/java/com/wangwei/OpenPlatformApplication.java
+++ src/main/java/com/wangwei/OpenPlatformApplication.java
@@ -1,10 +1,13 @@
 package com.wangwei;
 
+
+import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class OpenPlatformApplication {
+	private static final Logger logger = Logger.getLogger(OpenPlatformApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(OpenPlatformApplication.class, args);
